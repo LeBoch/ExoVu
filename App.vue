0d35c7ed3102750c1d5de14ed1d481fcc98387e7
Boch <118819662+LeBoch@users.noreply.github.com>
2024-03-27 18:13:07 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,55 @@
+
+<template>
+ 
+  <!-- <div v-for="n in userLength">  
+  <HumanEditor :userLength="userLength ">
+ </HumanEditor>
+  </div> -->
+ 
+   <div>
+     <div v-for="value in listUser" :key="value">
+      {{ value.name }}  {{ value.firstname }}  {{ value.age }}
+      <HumanEditor :name="value.name" :firstName="value.firstname" :age="value.age" @emitName="value.name = $event" @emitFirstName="value.firstname = $event" @emitAge="value.age = $event" ></HumanEditor>
+     </div>
+   </div>
+ </template>
+ 
+ <script setup>
+ import HumanEditor from './components/HumanEditor.vue'
+ import {reactive , ref} from "vue"
+ const listUser = reactive([
+   { "name": "Dupont", "firstname": "Jean", "age": 45 },
+   { "name": "Martin", "firstname": "Pierre", "age": 30 },
+   { "name": "Lefevre", "firstname": "Julie", "age": 25 },
+   { "name": "Lemoine", "firstname": "Sophie", "age": 35 },
+   { "name": "Moreau", "firstname": "Lucas", "age": 20 },
+   { "name": "Simon", "firstname": "Paul", "age": 50 },
+   { "name": "Laurent", "firstname": "Marie", "age": 40 },
+   { "name": "Leroy", "firstname": "Camille", "age": 33 },
+   { "name": "Roux", "firstname": "Alexandre", "age": 29 },
+   { "name": "Bertrand", "firstname": "Nicolas", "age": 39 },
+   { "name": "Girard", "firstname": "Charlotte", "age": 22 },
+   { "name": "Lambert", "firstname": "Thomas", "age": 44 },
+   { "name": "Rousseau", "firstname": "Maxime", "age": 31 },
+   { "name": "Clement", "firstname": "Chloe", "age": 28 },
+   { "name": "Lefebvre", "firstname": "Mathilde", "age": 37 },
+   { "name": "Dumas", "firstname": "Quentin", "age": 41 },
+   { "name": "Blanc", "firstname": "Justine", "age": 36 },
+   { "name": "Guerin", "firstname": "Clément", "age": 32 },
+   { "name": "Morin", "firstname": "Adrien", "age": 38 },
+   { "name": "Gauthier", "firstname": "Maëlle", "age": 34 },
+   { "name": "Perrin", "firstname": "Amandine", "age": 27 },
+   { "name": "Chevalier", "firstname": "Théo", "age": 46 },
+   { "name": "Francois", "firstname": "Valentin", "age": 43 },
+   { "name": "Legrand", "firstname": "Axel", "age": 47 },
+   { "name": "Gautier", "firstname": "Léa", "age": 23 },
+   { "name": "Marchand", "firstname": "Anaïs", "age": 26 },
+   { "name": "Marie", "firstname": "Lola", "age": 24 },
+   { "name": "Dubois", "firstname": "Benoît", "age": 42 },
+   { "name": "Dufour", "firstname": "Antoine", "age": 48 },
+   { "name": "David", "firstname": "Alexis", "age": 49 }
+ ]);
+ const userLength = ref(listUser.length)
+ 
+ </script>
+ 
---
+++ components/HumanEditor.vue
@@ -0,0 +1,54 @@
+<template>
+    <!-- <div id="range" class="demo">
+        <div v-for="n in props.userLength">  
+            <input type="text" v-model="name" class="Name">
+            <input type="text" v-model="firstName" class="FirstName">
+            <input type="Number" v-model="age" class="Age"> 
+            <button @click="envoyer()"> Envoyer </button>
+            {{  n }}
+        </div>
+    </div> -->
+
+    <div id="range" class="demo">
+    <input type="text" v-model="promptName" :placeholder="promptName" class="Name">
+    <input type="text" v-model="promptFirstName" :placeholder="promptFirstName" class="FirstName">
+    <input type="Number" v-model="pomptAge" :placeholder="promptAge" class="Age"> 
+    <button @click="send()"> Modifer </button>
+</div>
+
+</template>
+
+
+<script setup>
+import {defineProps , defineEmits , ref} from "vue"
+const props = defineProps(['name', 'firstName' , 'age' ])
+const emit = defineEmits(['emitName'] , ['emitFirstName'], ['emitAge'])
+const promptName = ref(props.name)
+const promptFirstName = ref(props.firstName)
+const promptAge = ref(props.age)
+
+
+function send() {
+    // if(!promptName.value || !promptName.value || !promptAge.value ) {
+    //     error.value = true
+    // }else{
+    //     open.value = false
+    //     error.value = false
+
+        emit('emitName' , promptName.value)
+        emit('emitFirstName' , promptFirstName.value)
+        emit('emitAge' , promptAge.value)
+
+    }
+    // !user.value ? (error.value = true) : (open.value = false , error.value = false , emit('send' , user.value ) );
+// }
+// const emit = defineEmits(['close'])
+// const name = ref('')
+// const firstName = ref('')
+// const age = ref('')
+
+
+// function envoyer() {
+    
+// }
+</script>
